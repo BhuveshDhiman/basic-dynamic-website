f00a49190bf31f349fa92b41dc566685d2bc7eaa
BhuveshDhiman <67685811+BhuveshDhiman@users.noreply.github.com>
2020-09-27 20:38:39 +0530

    Improved Website Version 0.2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 function getInput() {
+  document.getElementById('outputView').style.display = 'flex';
   var inputText = document.getElementById('input_text').value;
   console.log(inputText);
   var textColor = document.getElementById('text_color').value;
@@ -11,6 +12,21 @@ function getInput() {
   //alert(op);
   document.getElementById('op').innerHTML = inputText;
   document.getElementById('op').style.color = textColor;
+  document.getElementById('opimg').style.display = 'inline';
   document.getElementById('opimg').src = imageUrl;
-  document.getElementById('op').style.fontSize = fontSize;
+
+  document.getElementById('op').style.fontSize = fontSize + 'px';
+}
+
+function imageError() {
+  var imageUrl = document.getElementById('image_url').value;
+
+  if (imageUrl == '') {
+    document.getElementById('opimg').style.display = 'none';
+  } else {
+    document.getElementById('opimg').style.display = 'none';
+    alert('Please enter a valid image URL');
+  }
+
+  // style.display = 'none';
 }
